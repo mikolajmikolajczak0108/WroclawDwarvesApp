a3dbcb65aa7897bac90e4ed1e3b220cacb23a4be
mikolajmikolajczak0108 <116718075+mikolajmikolajczak0108@users.noreply.github.com>
2022-11-28 12:12:37 +0100

    basic debug in yaml

--- lib/pages/search_dwarves_page.dart
+++ lib/pages/search_dwarves_page.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class SearchDwarvesScreen extends StatefulWidget {
+  const SearchDwarvesScreen({super.key});
+
+  @override
+  State<SearchDwarvesScreen> createState() => _SearchDwarvesScreenState();
+}
+
+class _SearchDwarvesScreenState extends State<SearchDwarvesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
