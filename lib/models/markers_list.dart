6822e0cbd6b32e6474635905867304a0076a5b00
mikolajmikolajczak0108 <116718075+mikolajmikolajczak0108@users.noreply.github.com>
2022-12-06 21:54:40 +0100

    Fix: Deleted default bitmaperkarker

--- lib/models/markers_list.dart
+++ lib/models/markers_list.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 Future<List<Marker>> returnMarkers() async {
-  BitmapDescriptor markerIcon = BitmapDescriptor.defaultMarker;
-
   final dwarfIcon = await BitmapDescriptor.fromAssetImage(
       const ImageConfiguration(), "assets/images/dwarf_marker.png");
 
