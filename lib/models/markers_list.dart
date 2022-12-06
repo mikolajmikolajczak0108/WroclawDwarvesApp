63a181d66af01ca686a0250bade740b016a84e6f
Klaudiusz Dabrowski <klaudiuszdabrowski@arcsoftware.it>
2022-12-06 21:40:31 +0100

    Fixed markers list display

--- lib/models/markers_list.dart
+++ lib/models/markers_list.dart
@@ -1,12 +1,18 @@
+import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-List<Marker> returnMarkers(MarkerIcon) {
-  List<Marker> test = [
+Future<List<Marker>> returnMarkers() async {
+  BitmapDescriptor markerIcon = BitmapDescriptor.defaultMarker;
+
+  final dwarfIcon = await BitmapDescriptor.fromAssetImage(
+      const ImageConfiguration(), "assets/images/dwarf_marker.png");
+
+  return [
     Marker(
       draggable: true,
       markerId: const MarkerId("2"),
       position: LatLng(51.127398911595435, 16.978873217509076),
-      icon: MarkerIcon,
+      icon: dwarfIcon,
       infoWindow: const InfoWindow(
         title: 'Krasnal Nokiusz',
       ),
@@ -15,11 +21,10 @@ List<Marker> returnMarkers(MarkerIcon) {
       draggable: true,
       markerId: const MarkerId("3"),
       position: LatLng(51.12690240832611, 16.991319987951613),
-      icon: MarkerIcon,
+      icon: dwarfIcon,
       infoWindow: const InfoWindow(
         title: 'Krasnal Popowicjusz',
       ),
     ),
   ];
-  return test;
 }
